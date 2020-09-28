84c37208038a219c2bcba68b1e6869b82c21c5f8
MnoppawanD <72033967+MnoppawanD@users.noreply.github.com>
2020-09-28 23:09:23 +0700

    webconstruct2

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1601304274,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/main-sheet0.png",
+		"images/next-sheet0.png",
+		"images/p1-sheet0.png",
+		"images/back-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
